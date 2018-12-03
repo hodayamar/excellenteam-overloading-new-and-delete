2dc1cf6dfaff9fcd1f5e7dc37fa2abd8fda2e705
Hodaya Marciano <hodayamar2000@gmail.com>
2018-12-03 11:13:11 -0800

    header file

---
+++ person.h
@@ -0,0 +1,33 @@
+#ifndef EXCELLENTEAM_ELLA_CPP_OVERLOADING_NEW_AND_DELETE_HODAYAMAR_PERSON_H
+#define EXCELLENTEAM_ELLA_CPP_OVERLOADING_NEW_AND_DELETE_HODAYAMAR_PERSON_H
+
+#include <iostream>
+
+class Person {
+
+public:
+
+    static void* s_pool;
+    static void* s_firstFree;
+
+    Person();
+
+    ~Person();
+
+    void *operator new(size_t size);
+
+    void operator delete(void *ptr);
+
+private:
+
+    char full_name[32];
+
+    unsigned int id;
+    unsigned char age;
+
+    void* operator new[](size_t size);
+
+    void operator delete[](void *ptr);
+};
+
+#endif //EXCELLENTEAM_ELLA_CPP_OVERLOADING_NEW_AND_DELETE_HODAYAMAR_PERSON_H
